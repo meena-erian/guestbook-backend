373499149b4112827d1e50de428acb77edbb961e
Meena Erian <meena5.erian@gmail.com>
2020-03-22 19:40:35 +0200

    add user authentication endpoints
    Now the endpoints /user/login and /user/signup are wirking and tested

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
 var express = require("express");
-var md5 = require('md5');
-var uniqid = require('uniqid');
+var md5 = require("md5");
+var uniqid = require("uniqid");
 var cookieParser = require("cookie-parser");
 var app = express();
 //var fs = require("fs");
@@ -14,17 +14,89 @@ let validUsername = /^[a-zA-Z0-9]{5,30}$/;
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   dbo = db.db("guestbookdb");
-  app.get("/user/:username", function(req, res){
-
-    req.params.username
+  app.get("/user/:username", function(req, res) {
+    req.params.username;
   });
   app.post("/user/login", function(req, res) {
     res.setHeader("Content-Type", "application/json");
-    if(!req.body.username || !req.body.password || !validUsername.test(req.body.username)){
+    if (
+      !req.body.username ||
+      !req.body.password ||
+      !validUsername.test(req.body.username)
+    ) {
       res.status(401).end();
     }
-    
-    res.end(JSON.stringify({id : md5(uniqid())}));
+    dbo
+      .collection("users")
+      .findOne(
+        { username: req.body.username, password: req.body.password },
+        function(err, findRes) {
+          if (err) throw err;
+          if (findRes) {
+            let sid = md5(uniqid());
+            dbo.collection("sessions").insertOne(
+              {
+                sessionId: sid,
+                username: req.body.username,
+                userId: findRes._id,
+                start: new Date().getTime()
+              },
+              function(err, sessionInsertRes) {
+                if (err) throw err;
+                res.status(200).end(JSON.stringify({ id: sid }));
+              }
+            );
+          } else {
+            res.status(401).end();
+          }
+        }
+      );
+  });
+
+  app.post("/user/signup", function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    if (
+      !req.body.username ||
+      !req.body.password ||
+      !validUsername.test(req.body.username)
+    ) {
+      res.status(401).end();
+    }
+    dbo
+      .collection("users")
+      .findOne(
+        { username: req.body.username, password: req.body.password },
+        { username: true },
+        function(err, findRes) {
+          if (err) throw err;
+          if (findRes) {
+            res.status(451).end('{ "error" : "Username already in use" }');
+            console.log(findRes);
+          } else {
+            dbo.collection("users").insertOne(
+              {
+                username: req.body.username,
+                password: req.body.password,
+                registered: new Date().getTime()
+              },
+              function(err, InsertRes) {
+                if (err) {
+                  throw err;
+                } else {
+                  let sid = md5(uniqid());
+                  dbo.collection("sessions").insertOne({
+                    sessionId: sid,
+                    username: req.body.username,
+                    userId: InsertRes._id,
+                    start: new Date().getTime()
+                  });
+                  res.status(200).end(JSON.stringify({ id: sid }));
+                }
+              }
+            );
+          }
+        }
+      );
   });
 
   var server = app.listen(8080, function() {
