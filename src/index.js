73197fe5ab10be674e9b81cac9e01c44e36c1a3a
Meena Erian <meena5.erian@gmail.com>
2020-03-23 21:03:19 +0200

    add the methods and headers that will be used

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
 let validUsername = /^[a-zA-Z0-9]{5,30}$/;
